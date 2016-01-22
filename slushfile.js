95d2bf571b892e5209edc8f200ca2bbc02fcb373
Steve Silvestri <steve.silvestri@digitalreasoning.com>
2016-01-22 17:10:11 -0500

    typo fixes

--- slushfile.js
+++ slushfile.js
@@ -86,6 +86,7 @@ gulp.task('default', function (done) {
                 return done();
             }
             answers.appNameSlug = _.slugify(answers.appName);
+            gulp.src(__dirname + '/templates/.bowerrc').pipe(gulp.dest('./'));
             gulp.src(__dirname + '/templates/**')
                 .pipe(template(answers))
                 .pipe(rename(function (file) {
